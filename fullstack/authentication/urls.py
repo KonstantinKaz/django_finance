e6ccf475158cfc342f0027cd46a3320a5a4811eb
KonstantinKaz <kaz.world.work@mail.ru>
2023-06-18 21:07:56 +0300

    add authentication

---
+++ fullstack/authentication/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('login/', login_user, name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
+
+]
+
---
+++ fullstack/authentication/views.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+
+from .forms import LoginForm, RegisterForm
+from django.views.generic import TemplateView
+
+def login_user(request):
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+        else:
+            context = {
+                'login_form': login_form,
+            }
+
+    return render(request, 'authentication/login.html', context)
+
+
+class RegisterView(TemplateView):
+    template_name = 'authentication/register.html'
+
+    def get(self,request):
+        user_form = RegisterForm()
+        context = {'user_form': user_form}
+        return render(request, 'authentication/register.html', context)
+
+    def post(self,request):
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            login(request, user)
+            return redirect('index')
+        context = {'user_form': user_form}
+        return render(request, 'authentication/register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+# Create your views here.
